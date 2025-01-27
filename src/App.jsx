d7a7ce53cde7bf244812fc97faadefd5b118ab92
Mighty-Sarthak-07 <sarthak230504@gmail.com>
2025-01-28 00:43:09 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Hero from './components/Hero';
 import React from 'react';
 import './App.css';
 import About from './components/About';
+import NavBar from './components/Navbar';
 
 function App() {
 
   return (
     <>
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <NavBar/>
     <Hero/>
     <About/>
     </main>
